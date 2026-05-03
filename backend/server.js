f65df648f8e0ea2a3c46e34b64e09cd3a840a305
princeraj18 <princekr497@gmail.com>
2026-05-03 23:59:37 +0530

    added vercel json

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,8 @@ import authRoute from './src/routes/auth.route.js'
 const app = express()
 
 app.use(cors({
-    origin: true,
-    credentials: true
+    origin:ENV.CLIENT_URL ,
+    credentials:true
 }))
 app.use(cookieParser())
 app.use(express.json())
